c267270b2c2f4cac4fa723f970b8e075feecbd9f
BlueRose <378100977@qq.com>
2018-05-06 21:12:18 +0800

    有点问题

--- RayTrackingStudy.cpp
+++ RayTrackingStudy.cpp
@@ -14,14 +14,22 @@ bool hit_sphere(const vec3& center,float radius,const ray& r) {
 	//t*t*dot(B​,B​) + 2*t*dot(B,A​-C​) + dot(A-C,A​-C​) - R*R = 0
 	//t可以看作为x，也就是ax^2+bx+c=0，一个二次函数，所以为了判断光线是否与球相交只需要判断b^2-4ac是否大于0即可
 	float discriminant = b*b - 4 * a*c;
-	return (discriminant > 0);
+	if (discriminant < 0){
+		return -1.0;
+	}else{
+		return (-b - sqrt(discriminant)) / (2.0*a);
+	}
 }
 
 vec3 color(const ray& r) {
-	if (hit_sphere(vec3(0, 0, -1), 0.5, r))
-		return vec3(1, 0, 0);
+	float t=hit_sphere(vec3(0,0,-1),0.5,r);
+	if (t > 0.0) {
+		vec3 N = unit_vector(r.point_at_parameter(t) - vec3(0, 0, -1));
+		//return 0.5*vec3(N.x() + 1, N.y() + 1, N.z() + 1);
+		return vec3(1.0, 0, 0);
+	}
 	vec3 unit_direction = unit_vector(r.direction());
-	float t = 0.5*(unit_direction.y() + 1.0);
+	t = 0.5*(unit_direction.y() + 1.0);
 	return (1.0 - t)*vec3(1.0, 1.0, 1.0) + t*vec3(0.5, 0.7, 1.0);
 }
 
