9e0e395ca6c4f93e60e2f95b7de27096ba6ee186
bluerose <378100977@qq.com>
2018-05-07 15:41:37 +0800

    1、完成多个物体的渲染类，但是有问题需要解决

--- RayTrackingStudy.cpp
+++ RayTrackingStudy.cpp
@@ -4,33 +4,20 @@
 #include "stdafx.h"
 #include <iostream>
 #include <fstream>
-#include "ray.h"
+#include "hitable_list.h"
+#include "float.h"
+#include "sphere.h"
 
-double hit_sphere(const vec3& center,double radius,const ray& r) {
-	vec3 oc = r.origin() - center;
-	double a = dot(r.direction(), r.direction());
-	double b = 2.0*dot(oc, r.direction());
-	double c = dot(oc, oc) - radius*radius;
-	//t*t*dot(B​,B​) + 2*t*dot(B,A​-C​) + dot(A-C,A​-C​) - R*R = 0
-	//t可以看作为x，也就是ax^2+bx+c=0，一个二次函数，所以为了判断光线是否与球相交只需要判断b^2-4ac是否大于0即可
-	double discriminant = b*b - 4 * a*c;
-	if (discriminant < 0){
-		return -1.0;
-	}else{
-		return (-b - sqrt(discriminant)) / (2.0*a);
+vec3 color(const ray& r,hitable *world) {
+	hit_record rec;
+	if (world->hit(r, 0.0, DBL_MAX, rec)) {
+		return 0.5*vec3(rec.normal.x() + 1, rec.normal.y() + 1, rec.normal.z() + 1);
 	}
-}
-
-vec3 color(const ray& r) {
-	double t=hit_sphere(vec3(0,0,-1),0.5,r);
-	if (t > 0.0) {
-		vec3 N = unit_vector(r.point_at_parameter(t) - vec3(0, 0, -1));
-		return 0.5*vec3(N.x() + 1, N.y() + 1, N.z() + 1);
-		//return vec3(1.0, 0, 0);
+	else {
+		vec3 unit_direction = unit_vector(r.direction());
+		double t = 0.5*(unit_direction.y() + 1.0);
+		return (1.0 - t)*vec3(1.0, 1.0, 1.0) + t*vec3(0.5, 0.7, 1.0);
 	}
-	vec3 unit_direction = unit_vector(r.direction());
-	t = 0.5*(unit_direction.y() + 1.0);
-	return (1.0 - t)*vec3(1.0, 1.0, 1.0) + t*vec3(0.5, 0.7, 1.0);
 }
 
 int main()
@@ -47,6 +34,11 @@ int main()
 	vec3 vertical(0.0, 2.0, 0.0);
 	vec3 origin(0.0, 0.0, 0.0);
 
+	hitable *list[2];
+	list[0] = new sphere(vec3(0, 0, -1), 0.5);
+	list[1] = new sphere(vec3(0, -100.5, -1), 100);
+	hitable *world = new hitable_list(list, 2);
+
 	for (int j = ny - 1; j >= 0; j--)
 	{
 		for (int i=0;i<nx;i++)
@@ -54,8 +46,8 @@ int main()
 			double u = double(i) / double(nx);
 			double v = double(j) / double(ny);
 			ray r(origin,lower_left_corner+u*horizontal+v*vertical);
-			vec3 col = color(r);
-			//vec3 col(double(i) / double(nx), double(j) / double(ny), 0.2);
+			vec3 p = r.point_at_parameter(2.0);
+			vec3 col = color(r, world);
 
 			int ir = int(255.99*col[0]);
 			int ig = int(255.99*col[1]);
