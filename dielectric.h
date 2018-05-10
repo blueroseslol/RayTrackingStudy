af962d310c0c5f7cf4db3cc703bbd415e16899e2
bluerose <378100977@qq.com>
2018-05-10 15:07:25 +0800

    Revert "1、折射代码完成。"
    This reverts commit 64277fd29a61617779ca957c8e4ff4c1ec6aa82c.

--- dielectric.h
+++ dielectric.h
@@ -1,11 +1,5 @@
 #pragma once
 #include "material.h"
-double schlick(double cosine,double ref_idx) {
-	double r0 = (1 - ref_idx) / (1 + ref_idx);
-	r0 = r0*r0;
-	return r0 + (1 - r0)*pow(1 - cosine, 5);
-}
-
 bool refract(const vec3& v, const vec3& n, double ni_over_net, vec3& refracted) {
 	vec3 uv = unit_vector(v);
 	double dt = dot(uv, n);
@@ -29,39 +23,37 @@ public:
 		double ni_over_nt;
 		attenuation = vec3(1.0, 1.0, 1.0);
 		vec3 refracted;
-		double reflect_prob;
-		double cosine;
-		//入射光线
+		//double reflect_prob;
+		//double cosine;
 		if (dot(r_in.direction(), rec.normal) > 0) {
 			outward_normal = -rec.normal;
 			ni_over_nt = ref_idx;
-			cosine = ref_idx*dot(r_in.direction(), rec.normal) / r_in.direction().length();
+	/*		cosine = ref_idx*dot(r_in.direction(), rec.normal) / r_in.direction().length();*/
 		}else{
-			//出射光线
 			outward_normal = rec.normal;
 			ni_over_nt = 1.0 / ref_idx;
-			cosine = -dot(r_in.direction(), rec.normal) / r_in.direction().length();
+			//cosine = -dot(r_in.direction(), rec.normal) / r_in.direction().length();
 		}
-		if (refract(r_in.direction(),outward_normal,ni_over_nt,refracted)){
-			reflect_prob = schlick(cosine, ref_idx);
-		}
-		else {
-			scattered = ray(rec.p, refracted);
-			reflect_prob = 1.0;
-		}
-		if (uni_dist(reng) < reflect_prob) {
-			scattered = ray(rec.p, reflected);
-		}
-		else {
-			scattered = ray(rec.p, refracted);
-		}
-		//if (refract(r_in.direction(), outward_normal, ni_over_nt, refracted)) {
-		//	scattered = ray(rec.p, refracted);
+		//if (refract(r_in.direction(),outward_normal,ni_over_nt,refracted)){
+		//	reflect_prob = schlick(cosine, ref_idx);
 		//}
 		//else {
-		//	scattered = ray(rec.p, reflected);
-		//	return false;
+		//	scattered = ray(rec.p, refracted);
+		//	reflect_prob = 1.0;
 		//}
+		//if (uni_dist(reng) < reflect_prob) {
+		//	scattered = ray(rec.p, reflected);
+		//}
+		//else {
+		//	scattered = ray(rec.p, refracted);
+		//}
+		if (refract(r_in.direction(), outward_normal, ni_over_nt, refracted)) {
+			scattered = ray(rec.p, refracted);
+		}
+		else {
+			scattered = ray(rec.p, reflected);
+			return false;
+		}
 		return true;
 	}
 	double ref_idx;
