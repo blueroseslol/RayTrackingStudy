3e6bcaceb9d214ee8f13ba60c42638bbc737e5e6
bluerose <378100977@qq.com>
2018-05-14 17:45:26 +0800

    1、开始the next week学习

--- ray.h
+++ ray.h
@@ -5,13 +5,15 @@ class ray
 public:
 	//p​(t) = A​ + t*B
 	ray(){}
-	ray(const vec3& a, const vec3& b) { A = a; B = b; }
+	ray(const vec3& a, const vec3& b, double ti = 0) { A = a; B = b; _time = ti; }
 
 	vec3 origin() const { return A; }
 	vec3 direction() const { return B; }
+	double time() const { return _time; }
 	vec3 point_at_parameter(double t) const { return A + t*B; }
 
 	vec3 A;
 	vec3 B;
+	double _time;
 };
 
